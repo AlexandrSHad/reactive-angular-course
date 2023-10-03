a94191c50493bd3d9c0ad2f4e190e4c3354e93af
Oleksandr Shadurskyi <shadurskij@gmail.com>
2023-10-03 19:40:38 +0200

    added back-end error handling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,6 +14,7 @@ import {
 } from "rxjs/operators";
 import { CoursesService } from "../services/courses.service";
 import { LoadingService } from "../loading/loading.service";
+import { MessagesService } from "../messages/messages.service";
 
 @Component({
   selector: "home",
@@ -27,7 +28,8 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private coursesService: CoursesService,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
   ) {}
 
   ngOnInit() {
@@ -35,9 +37,15 @@ export class HomeComponent implements OnInit {
   }
 
   reloadCourses() {
-    const courses$ = this.coursesService
-      .loadAllCourses()
-      .pipe(map((courses) => courses.sort(sortCoursesBySeqNo)));
+    const courses$ = this.coursesService.loadAllCourses().pipe(
+      map((courses) => courses.sort(sortCoursesBySeqNo)),
+      catchError((err) => {
+        const message = "Could not load courses";
+        this.messagesService.showErrors(message);
+        console.log(message, err);
+        return throwError(err);
+      })
+    );
 
     const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
 
